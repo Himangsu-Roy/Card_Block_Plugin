78a0b5cef97fcd10e3d599887424668e1fdbd2b1
Himu Roy <himur98@gmail.com>
2024-12-26 19:43:26 +0600

    added some

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7bf58cdc0baf762ad645');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ae3c44dca1ef2ca5b32c');
